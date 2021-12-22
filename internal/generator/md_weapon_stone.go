eacd2f9c4a21b78845ae7f10cba0c11bc59f19a3
binaryeater <>
2021-12-22 04:25:53 +0000

    Fix MD_WeaponStone json annotation

--- internal/generator/md_weapon_stone.go
+++ internal/generator/md_weapon_stone.go
@@ -2,7 +2,7 @@ package generator
 
 type MD_WeaponStone struct {
 	ID     string `json:"_ID"`
-	Name   string `json:"_String"`
+	Name   string `json:"_Name"`
 	Detail string `json:"_Detail"`
 	Rarity int    `json:"_Rarity"`
 }
